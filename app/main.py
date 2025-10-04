eb0ab29a02e4db8b14d25e7cec37ea78fe038b58
ivanchetvergov <ivar5165885@gmail.com>
2025-10-04 20:03:27 +0300

    add: FastAPI app

---
+++ app/main.py
@@ -0,0 +1,18 @@
+# app/main.py
+
+from fastapi import FastAPI
+from app.api.endpoints import train, task
+
+app = FastAPI(
+    title="ML-Lab API:",
+    description="API для асинхронного обучения и сравнения моделей машинного обучения.",
+    version="0.1.0"
+)
+
+app.include_router(train.router, prefix="/api", tags=["Training"])
+app.include_router(task.router, prefix="/api", tags=["Task Status"])
+
+@app.get("/")
+async def root():
+    # Здесь можно сделать простой HTML-интерфейс, как мы обсуждали
+    return {"message": "Welcome to ML-Lab API. Access /docs for Swagger UI."}
