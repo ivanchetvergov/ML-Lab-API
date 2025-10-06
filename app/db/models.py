6992d8d21cd6377de5e8c3bcb9555679833d4ca1
ivanchetvergov <ivar5165885@gmail.com>
2025-10-06 21:54:54 +0300

    add: db models(Task, Model)

--- app/db/models.py
+++ app/db/models.py
@@ -1,30 +1,60 @@
 # app/db/models.py
-from sqlalchemy import Column, Integer, String, Float, DateTime
-from sqlalchemy.orm import declarative_base
 from datetime import datetime
+from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey, Text, JSON
+from sqlalchemy.orm import relationship
 
-Base = declarative_base()
+from app.db.database import Base 
 
-class ModelResult(Base):
-    __tablename__ = "model_results"
+class Task(Base):
+    """
+    Модель для таблицы 'tasks'. 
+    Отслеживает все запущенные задачи обучения (PENDING, SUCCESS, FAILURE).
+    """
+    __tablename__ = "tasks"
+
+    # Основной ID задачи для внешнего API
+    id = Column(Integer, primary_key=True, index=True) 
     
-    # --- короткий ID для user'a
-    id = Column(Integer, primary_key=True, index=True)
+    # Полный UUID задачи Celery для внутреннего мониторинга
+    celery_task_id = Column(String(36), unique=True, index=True, nullable=False)
     
-    # --- ID задачи Celery
-    celery_task_id = Column(String, unique=True, index=True, nullable=False)
-    
-    # --- metadata
+    # Метаданные запроса
     model_type = Column(String, nullable=False)
     data_id = Column(String, nullable=False)
+    params_json = Column(JSON, default={}) # спользуем тип JSONB в PostgreSQL
     
-    # --- result
-    accuracy = Column(Float, nullable=True)
-    f1_score = Column(Float, nullable=True)
+    # Статус и время
+    submitted_at = Column(DateTime, default=datetime.utcnow)
+    status = Column(String, default="PENDING", nullable=False) 
+    error_message = Column(Text, nullable=True)
     
-    # --- path
+    # Внешний ключ
+    final_model_id = Column(Integer, ForeignKey("models.id"), nullable=True)
+
+    # Relationship: Позволяет обращаться к модели как к атрибуту (task.result_model)
+    result_model = relationship("Model", back_populates="source_task", foreign_keys=[final_model_id])
+
+
+class Model(Base):
+    """
+    Модель для таблицы 'models'. 
+    Каталог только для успешно обученных моделей, готовых к использованию.
+    """
+    __tablename__ = "models"
+
+    # ID модели для Predict API
+    id = Column(Integer, primary_key=True, index=True)
+    
+    # Внешний ключ, который ссылается на исходную задачу
+    task_id = Column(Integer, ForeignKey("tasks.id"), unique=True, nullable=False)
+    
+    # Результаты
+    accuracy = Column(Float, nullable=False)
+    f1_score = Column(Float, nullable=False)
+    
+    # Путь к сохраненному файлу
+    model_path = Column(String(255), unique=True, nullable=False)
     trained_at = Column(DateTime, default=datetime.utcnow)
-    
-    # --- task status (PENDING, PROGRESS, SUCESS, FAILURE)
-    status = Column(String, default="PENDING", nullable=False)
-    
+
+    # Relationship: Позволяет обращаться к задаче как к атрибуту (model.source_task)
+    source_task = relationship("Task", back_populates="result_model", foreign_keys=[task_id])
